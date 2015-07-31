d3b9d27789cc8818c1e3dc8676eb69c39cdd679c
Caleb Mosher <caleb_mosher@apple.com>
2015-07-31 16:31:29 -0700

    Add state polling

--- public/scripts/test-cal.js
+++ public/scripts/test-cal.js
@@ -14,10 +14,7 @@ var data = [
 
 // Components
 var CommentBox = React.createClass({
-	getInitialState: function() {
-		return {data: [ ]};
-	},
-	componentDidMount: function() {
+	loadCommentsFromServer: function() {
 		$.ajax({
 			url: this.props.url,
 			dataType: "json",
@@ -29,6 +26,13 @@ var CommentBox = React.createClass({
 				console.error(this.props.url, status, err.toString());
 			}.bind(this)
 		});
+	},
+	getInitialState: function() {
+		return {data: [ ]};
+	},
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
 	},
 	render: function() {
 		return (
@@ -84,6 +88,6 @@ var Comment = React.createClass({
 
 
 React.render(
-	<CommentBox url="comments.json" />,
+	<CommentBox url="comments.json" pollInterval={2000} />,
 	document.getElementById("content")
 );
