6f9c2334310789878a64b173830e29828e8d5e98
Caleb Mosher <caleb_mosher@apple.com>
2015-07-31 16:04:03 -0700

    Init up to first ajax

---
+++ public/scripts/test-cal.js
@@ -0,0 +1,89 @@
+// Data
+var data = [
+	{
+		author: "Pete Hunt",
+		text: "Comment **bold**."
+	},
+	{
+		author: "Jordan Walke",
+		text: "Comment *italic*."
+	}
+];
+
+
+
+// Components
+var CommentBox = React.createClass({
+	getInitialState: function() {
+		return {data: [ ]};
+	},
+	componentDidMount: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: "json",
+			cache: false,
+			success: function(obj) {
+				this.setState({data: obj});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	render: function() {
+		return (
+			<div className="commentBox">
+				<h1>Comments</h1>
+				<CommentList data={this.state.data} />
+				<CommentForm />
+			</div>
+		);
+	}
+});
+
+var CommentList = React.createClass({
+	render: function() {
+		var commentNodes = this.props.data.map(function(comment) {
+			return (
+				<Comment author={comment.author}>
+					{comment.text}
+				</Comment>
+			);
+		});
+		return (
+			<div className="commentList">
+				{commentNodes}
+			</div>
+		);
+	}
+});
+
+var CommentForm = React.createClass({
+	render: function() {
+		return (
+			<div className="commentForm">
+				I am a CommentForm.
+			</div>
+		);
+	}
+});
+
+var Comment = React.createClass({
+	render: function() {
+		return (
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				{marked(this.props.children.toString(), {sanitize: true})}
+			</div>
+		);
+	}
+});
+
+
+
+React.render(
+	<CommentBox url="comments.json" />,
+	document.getElementById("content")
+);
